ebd3e5329ea25ce1476f80c8046f7d6c3505ce79
David González <dagonzalezce@unal.edu.co>
2022-05-21 16:22:43 -0500

    Submission

--- preguntas.py
+++ preguntas.py
@@ -16,16 +16,16 @@ def pregunta_01():
     Carga y separación de los datos en `X` `y`
     """
     # Lea el archivo `concrete.csv` y asignelo al DataFrame `df`
-    df = ____  
+    df = df.read_csv("concrete.csv")  
 
     # Asigne la columna `strength` a la variable `y`.
-    ____ = ____  
+    y = df["strength"]  
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)  
+    X = df.copy(deep=False)  
 
     # Remueva la columna `strength` del DataFrame `X`.
-    ____.____(____)  
+    X = X.drop(columns=["strength"])  
 
     # Retorne `X` y `y`
     return x, y
@@ -37,7 +37,7 @@ def pregunta_02():
     """
 
     # Importe train_test_split
-    from ____ import ____
+    from sklearn.model_selection import train_test_split
 
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     x, y = pregunta_01()
@@ -49,12 +49,13 @@ def pregunta_02():
         x_test,  
         y_train,  
         y_test,  
-    ) = ____(  
-        ____,  
-        ____,  
-        test_size=____,  
-        random_state=____,  
+    ) = train_test_split(  
+        X,  
+        y,  
+        test_size=0.25,  
+        random_state=12453,  
     )  
+
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
     return x_train, x_test, y_train, y_test
