3114fdeaa1b07bbb955f6b97b53c7e79646cd4bc
David González <dagonzalezce@unal.edu.co>
2022-06-20 09:30:18 -0500

    Submission

--- preguntas.py
+++ preguntas.py
@@ -125,7 +125,7 @@ def pregunta_04():
         estimator=estimator,
         param_grid=param_grid,
         cv = 5, 
-        scoring='accuracy'  
+        scoring='r2'  
     )
 
     return gridsearchcv
